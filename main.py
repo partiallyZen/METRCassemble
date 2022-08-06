9cfabe4de53ca1acb213c59ac795d7be5702a4f2
partiallyZen <42852344+partiallyZen@users.noreply.github.com>
2022-08-05 22:18:05 -0500

    Updating and Streamlined with Numpy
    1.0.2 Update

--- main.py
+++ main.py
@@ -1,31 +1,21 @@
 #!/usr/bin/env python3
 import imports
-from function import *
+from function import * # Yeah I am a weird guy who puts all his stuff in other files...
 import variablestore
-
 
 kevdate()
 x = kevdate()
 
-f = open(str(x) + "beta.csv", 'w')
-f.close
-
 while True:
   newrow = harvestrow()
-  print(tabulate(display, headers=["|METRC RFID Tag #|", "Weight", "Unit", "Location Name", "Harvest Name", 'blank', "Today's Date"], tablefmt="grid"))
-  f = open(str(x) + "beta.csv", 'a') #finalize the measurement
-  writer = csv.writer(f) # open writer
-  writer.writerow(newrow) # write row or write all     rows...havent decided
-  f.close #closer 'er up
+  kevcount()
+  print(tabulate(display, headers=["RFID Tag #", "Weight", "Unit", "Location", "Harvest", 'blank', "Date"], tablefmt="grid")) # Format Pretty Table
   truthcheck = input("Are you finished? [Y or N]: ")
-  if truthcheck.upper() == "Y":
-    print("Your METRC compatible CSV file is complete")
+  if truthcheck.upper() == "Y": # Need a loop for multi-row input
+    print("Your METRC compatible CSV file is compiling...") 
+    np.savetxt(str(x)+"beta.csv", display, delimiter=", ", fmt="% s") # Numpy is superior to the csvwriter IMHO
+    for i in tqdm(range(3)): # Classic wait so as not to confused anyone...
+      sleep(2)
+    print("Your METRC compatible " + str(x) + "beta.csv is complete.")
     print("Thanks for using the METRC assembler!")
     break
-
-# harvestrow = (rfidscan(), scalewght(), "grams", harvestloc(), harvestnom(), '', kevdate())
-
-#f = open(str(x) + "beta.csv", 'a') #finalize the measurement
-#writer = csv.writer(f) # open writer
-#writer.writerow(harvestrow) # write row or write all rows...havent decided
-#f.close #closer 'er up
