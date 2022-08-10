6a3fff639f4dcbe727b30e156c493734e86f49a9
partiallyZen <42852344+partiallyZen@users.noreply.github.com>
2022-08-10 13:29:34 -0500

    Updated to Current 1.0.1

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
-import imports
-from function import * # Yeah I am a weird guy who puts all his stuff in other files...
-import variablestore
+from imports import *
+from function import *
+from variablestore import *
+import serial
 
-kevdate()
 x = kevdate()
 
 while True:
   newrow = harvestrow()
-  kevcount()
-  print(tabulate(display, headers=["RFID Tag #", "Weight", "Unit", "Location", "Harvest", 'blank', "Date"], tablefmt="grid")) # Format Pretty Table
+  print(tabulate(display, headers=["Tag#", "Weight", "Unit", "Location", "Harvest", 'blank', "Date"], tablefmt="grid"))
   truthcheck = input("Are you finished? [Y or N]: ")
-  if truthcheck.upper() == "Y": # Need a loop for multi-row input
-    print("Your METRC compatible CSV file is compiling...") 
-    np.savetxt(str(x)+"beta.csv", display, delimiter=", ", fmt="% s") # Numpy is superior to the csvwriter IMHO
-    for i in tqdm(range(3)): # Classic wait so as not to confused anyone...
+  if truthcheck.upper() == "Y":
+    print("Your METRC compatible CSV file is compiling...")
+    np.savetxt(str(x)+"beta.csv", display, delimiter=", ", fmt="% s")
+    for i in tqdm(range(2)):
       sleep(2)
     print("Your METRC compatible " + str(x) + "beta.csv is complete.")
     print("Thanks for using the METRC assembler!")
     break
+
+
+# harvestrow = (rfidscan(), scalewght(), "grams", harvestloc(), harvestnom(), '', kevdate())
+
+#f = open(str(x) + "beta.csv", 'a') #finalize the measurement
+#writer = csv.writer(f) # open writer
+#writer.writerow(harvestrow) # write row or write all rows...havent decided
+#f.close #closer 'er up
