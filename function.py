d07450093e18c7ac1d91ced57d5bc8f0586faaec
partiallyZen <42852344+partiallyZen@users.noreply.github.com>
2022-08-10 13:28:47 -0500

    Removed Serial Weight to Own file
    Adding some input validation too

--- function.py
+++ function.py
@@ -2,51 +2,48 @@
 
 from imports import *
 from variablestore import *
+from pyserial import scaleweight
+
+
+
+def kevcount():
+    kevnum =+ 1
+    print("You have currently added " + str(kevnum) + " rows this session.")
+    return (kevnum)
+
+
+#def tabledisp():
+ #   print(tabulate(display, disphead, tablefmt="grid")
+  #  return
+
+
+def kevdate():
+    today = date.today().strftime('%Y-%m-%d')
+    screen_clear()
+    print("\nToday is " + today + ". Adding this to the METRC Table...\n")
+    sleep(3)
+    screen_clear()
+    return (today)
+
+
+def rfidscan():
+    RFIDTAG = input("Please scan METRC RFID TAG now...")
+    RFIDTAGs = str(RFIDTAG)
+    print("You have scanned Tag #: " + RFIDTAGs)
+    sleep(5)
+    screen_clear()
+    return (RFIDTAGs)  #add choice to rescan, or add to <metrcrow>
+
 
 def screen_clear():
     # for mac and linux(here, os.name is 'posix')
     if os.name == 'posix':
-        _ = os.system('clear')     # I love when menus clear and re-open...
+        _ = os.system('clear')
     else:
         # for windows platfrom
         _ = os.system('cls')
     # print out some text
 
-def kevdate():
-    today = date.today().strftime('%Y-%m-%d')
-    print("Today is " + today + ".")
-    print("Adding this information to METRC Table...") #kevdate with some flair
-    screen_clear()
-    return (today)
-
-def kevcount():
-    kevnum = len(display)
-    print("You have currently added " + str(kevnum) + " rows this session.")  #Prog'd for the testing, stays because I like it 
-    return (kevnum)
-
-def rfidscan():
-  while True:
-    RFIDTAG = input("Please scan METRC RFID TAG now...")
-    RFIDTAGs = str(RFIDTAG)
-    if int(len(RFIDTAGs)) != 24:                                      #RFID Scan and Length verification. [Will need to update with **args]
-      screen_clear()
-      print("ENTRY INVALID INTIALIZING RESCAN")
-    else:
-      print("You scanned valid METRC Tag: " + RFIDTAGs)
-      print("Adding this information to the table...")
-      time.sleep(2)
-      screen_clear()
-      return (RFIDTAGs)  #add choice to rescan, or add to <metrcrow> 
-      break
-
-def scalewght():
-    WGHT = input("Please press the ENTER key on scale | ")
-    # x=ser.readline()
-    # d=x.decode()    #prints out the number only                   uncomment these lines for serial input
-    # WGHT=d[5:10]
-    print("The scale returned " + WGHT + " grams")
-    return (WGHT)
-    # Needs input validations and testing
 
 def harvestloc():
     print(tabulate(table, headers = ["Numeral", "Room Name"], tablefmt="grid"))
@@ -54,17 +51,28 @@ def harvestloc():
     #print(printform)
     harvestloc0 = rooms[1]
 
-    print("Select Harvest Location by Numeral | Currently: " + harvestloc0)
-    x = input()
-    n = int(x)
-    harvestloc0 = rooms[n]
-    screen_clear()
-    print("You selected: " + harvestloc0)
-    return (harvestloc0)
+    print("Select Harvest Location by Numeral [1-4] | Currently: " + harvestloc0)
+    while True:
+        try:
+            value=int(input())
+        except ValueError:
+            print("Please Select a Number 1-4...")
+            continue
+        else:
+            harvestloc0 = rooms[value]
+            screen_clear()
+            print("You selected: " + harvestloc0)
+            sleep(3)                              #Need to add input validation, will throw errors with non int
+            screen_clear()
+            return (harvestloc0)
 
 
 def harvestnom():
     harvestnom0 = input("Type your Harvest Name here: ")
+    print("You have entered: " + harvestnom0)
+    print("Adding harvest date...")
+    sleep(5)
+    screen_clear()
     return (harvestnom0 + kevdate())
 
 
@@ -79,9 +87,12 @@ def harvestrow():
     #    Patient License Number {=USER?=}
     #    Actual Date {<today>}
 
-    harvestrow0 = (rfidscan(), scalewght(), "grams", harvestloc(),
+    harvestrow0 = (rfidscan(), scaleweight(), "grams", harvestloc(),
                    harvestnom(), '', kevdate())
     print("Printing the following row(s) to METRC Template")
     display.append(harvestrow0)
-    print(display)
+    x = str(len(display))
+    print("You have added " + x + " rows.")
+    screen_clear()
     return (harvestrow0)
+
